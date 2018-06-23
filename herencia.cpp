1ee808af46b43757954a2acc13605b89de23511d
Claudia Paredes <cpc1899@gmail.com>
2018-06-23 16:39:42 -0500

    herencia de animales

---
+++ herencia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal{
+    protected:
+        string nombre;
+        int patas;
+    public:
+        Animal(){nombre="";patas=0;}
+        ~Animal(){}
+};
+class Perro:public Animal{
+    public:
+        void sonido(){
+            cout<<"guau";
+        }
+};
+
+class Gato: public Animal{
+    public:
+        void sonido(){
+            cout<<"miau";
+        }
+};
