c8bf32ac011ca8af7d4e48fec8d1c6f6d33b7fd2
Claudia Paredes <cpc1899@gmail.com>
2018-06-22 23:00:33 -0500

    sintaxis

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Vector{ //nombre de la clase "Vector"
+    private:
+        int x;  //variables miembro
+        int y;
+    public:
+        void setx(int a);   //funciones miembro
+        void sety(int a);
+        int getx();
+        int gety();
+};
+
